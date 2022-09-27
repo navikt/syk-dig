2ecaf9d49f273b932ab53f0fab3d0e00cbce4e0e
Karl O <k@rl.run>
2022-09-27 08:58:42 +0100

    setup RTL (#9)

---
+++ src/utils/testUtils.tsx
@@ -0,0 +1,57 @@
+import { MockedProvider, MockedResponse } from '@apollo/client/testing';
+import { PropsWithChildren, ReactElement } from 'react';
+import { RenderOptions, render } from '@testing-library/react';
+import { Cache, InMemoryCache, TypedDocumentNode } from '@apollo/client';
+
+import { cacheConfig } from '../graphql/apollo';
+import { modiaLocalResolvers } from '../graphql/localState/modia';
+
+type ProviderProps = {
+    readonly initialState?: Cache.WriteQueryOptions<unknown, unknown>[];
+    readonly mocks?: MockedResponse[];
+};
+
+function AllTheProviders({ children, initialState, mocks }: PropsWithChildren<ProviderProps>): JSX.Element {
+    const cache = new InMemoryCache(cacheConfig);
+    initialState?.forEach((it) => cache.writeQuery(it));
+
+    return (
+        // TODO maybe use MSW?
+        <MockedProvider
+            mocks={mocks}
+            cache={cache}
+            resolvers={{
+                Mutation: {
+                    ...modiaLocalResolvers,
+                },
+            }}
+        >
+            {children}
+        </MockedProvider>
+    );
+}
+
+export function createInitialQuery<Query, Variables>(
+    typedDocumentNode: TypedDocumentNode<Query, Variables>,
+    data: Query,
+    variables?: Variables,
+): Cache.WriteQueryOptions<Query, Variables> {
+    return {
+        query: typedDocumentNode,
+        data,
+        variables,
+    };
+}
+
+const customRender = (
+    ui: ReactElement,
+    options?: Omit<RenderOptions, 'wrapper'> & ProviderProps,
+): ReturnType<typeof render> =>
+    render(ui, {
+        wrapper: (props) => <AllTheProviders {...props} initialState={options?.initialState} mocks={options?.mocks} />,
+        ...options,
+    });
+
+export * from '@testing-library/react';
+
+export { customRender as render };
