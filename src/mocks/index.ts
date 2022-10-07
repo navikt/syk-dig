a718395fc2ef1020085401b72a66dc264cd059d9
Karl J. Overå <k@rl.run>
2022-10-07 13:21:59 +0200

    fix country picker initial value bug

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,5 +1,8 @@
 async function initMocks(): Promise<void> {
     if (typeof window === 'undefined') {
+        // We don't use MSW for mocking server side in dev, only tests
+        if (process.env.NODE_ENV !== 'test') return;
+
         const { server } = await import('./server');
         server.listen();
     } else {
