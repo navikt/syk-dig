a000270199cca89c9a73bba9854eacf0ea60fe9e
Karl O <k@rl.run>
2022-10-10 13:13:42 +0100

    add action section (#20)

--- src/components/SykmeldingSection/SykmeldingSection.tsx
+++ src/components/SykmeldingSection/SykmeldingSection.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react';
 import { Heading } from '@navikt/ds-react';
 import { Calender } from '@navikt/ds-icons';
+import cn from 'clsx';
 
 import styles from './SykmeldingSection.module.css';
 
@@ -8,19 +9,32 @@ export interface Props {
     id?: string;
     Icon: typeof Calender;
     title: string;
+    variant?: 'blue' | 'light';
 }
 
-function SykmeldingSection({ id, Icon, title, children }: PropsWithChildren<Props>): JSX.Element {
+function SykmeldingSection({ id, Icon, title, children, variant = 'blue' }: PropsWithChildren<Props>): JSX.Element {
     return (
-        <section className={styles.section} aria-labelledby={id}>
-            <div className={styles.heading}>
-                <Icon aria-hidden />
-                <Heading level="2" size="medium" id={id}>
-                    {title}
-                </Heading>
-            </div>
+        <section className={cn(styles.section, { [styles.lightSection]: variant === 'light' })} aria-labelledby={id}>
+            <SectionHeader headingId={id} title={title} Icon={Icon} />
             {children}
         </section>
+    );
+}
+
+interface SectionHeaderProps {
+    headingId?: string;
+    title: string;
+    Icon: typeof Calender;
+}
+
+export function SectionHeader({ headingId, title, Icon }: SectionHeaderProps): JSX.Element {
+    return (
+        <div className={styles.heading}>
+            <Icon aria-hidden />
+            <Heading level="2" size="medium" id={headingId}>
+                {title}
+            </Heading>
+        </div>
     );
 }
 
