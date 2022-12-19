b7b750aeb79f015fa39409dd56f9ffb23cd8f41c
Karl J. Overå <k@rl.run>
2022-12-19 15:37:41 +0100

    update schema to new union oppgave API

---
+++ src/components/OppgaveStatus/OppgaveStatus.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Alert, BodyShort, Heading } from '@navikt/ds-react'
+
+import {
+    DigitaliseringsoppgaveStatusEnum,
+    DigitaliseringsoppgaveStatusFragment,
+} from '../../graphql/queries/graphql.generated'
+
+import styles from './OppgaveStatus.module.css'
+
+interface Props {
+    oppgave: DigitaliseringsoppgaveStatusFragment
+}
+
+function OppgaveStatus({ oppgave }: Props): JSX.Element {
+    switch (oppgave.status) {
+        case DigitaliseringsoppgaveStatusEnum.Ferdigstilt:
+            return (
+                <Alert variant="warning" className={styles.alert}>
+                    <Heading size="medium" spacing>
+                        Oppgaven er allerede ferdigstilt
+                    </Heading>
+                    <BodyShort spacing>
+                        Dersom du kom hit fra Gosys kan du prøve å oppfriske Gosys og prøve igjen.
+                    </BodyShort>
+                    <BodyShort spacing>Dersom problemet vedvarer kan du ta kontakt.</BodyShort>
+                </Alert>
+            )
+        case DigitaliseringsoppgaveStatusEnum.IkkeEnSykmelding:
+            return (
+                <Alert variant="warning" className={styles.alert}>
+                    <Heading size="medium" spacing>
+                        Oppgaven har blitt sendt tilbake til Gosys fordi det ikke var en sykmelding
+                    </Heading>
+                    <BodyShort spacing>
+                        Dersom du kom hit fra Gosys kan du prøve å oppfriske Gosys og prøve igjen.
+                    </BodyShort>
+                    <BodyShort spacing>Dersom problemet vedvarer kan du ta kontakt.</BodyShort>
+                </Alert>
+            )
+        case DigitaliseringsoppgaveStatusEnum.FinnesIkke:
+            return (
+                <Alert variant="warning" className={styles.alert}>
+                    <Heading size="medium" spacing>
+                        Vi klarte ikke å finne oppgaven
+                    </Heading>
+                    <BodyShort spacing>
+                        Oppgaven med oppgave-id: {oppgave.oppgaveId} finnes ikke i våre systemer.
+                    </BodyShort>
+                    <BodyShort spacing>
+                        Dersom du kom hit fra Gosys kan du prøve å oppfriske Gosys og prøve igjen.
+                    </BodyShort>
+                    <BodyShort spacing>Dersom problemet vedvarer kan du ta kontakt.</BodyShort>
+                </Alert>
+            )
+    }
+}
+
+export default OppgaveStatus
