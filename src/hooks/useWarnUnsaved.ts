fa08050f25b9a95ff189db94118745fda549a685
Karl J. Overå <k@rl.run>
2023-03-29 13:20:29 +0200

    bump RHF to fix dirty warn when leaving

--- src/hooks/useWarnUnsaved.ts
+++ src/hooks/useWarnUnsaved.ts
@@ -2,13 +2,14 @@ import { useEffect } from 'react'
 
 function useWarnUnsavedPopup(shouldWarn: boolean): void {
     useEffect(() => {
-        const handleBeforeUnload = (e: Event): void => {
+        const handleBeforeUnload = (e: BeforeUnloadEvent): void => {
             if (shouldWarn) {
                 e.preventDefault()
-                // @ts-expect-error setter returnValue til tom streng fordi https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeunload
+                // setter returnValue til tom streng fordi https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeunload
                 e.returnValue = ''
             }
         }
+
         window.addEventListener('beforeunload', handleBeforeUnload)
 
         return () => {
