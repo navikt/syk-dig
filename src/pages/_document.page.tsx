7242b7b8d2f55c2cc2a64bd8ab471418680ce8db
Karl J. Overå <k@rl.run>
2023-04-20 13:30:10 +0200

    use nextjs standalone output

---
+++ src/pages/_document.page.tsx
@@ -0,0 +1,17 @@
+import 'next-logger'
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+function Document(): JSX.Element {
+    return (
+        <Html>
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
+
+export default Document
