08026256cf574ccfd590969e954f7676e19e5dc1
Karl J. Overå <k@rl.run>
2023-05-26 12:26:41 +0200

    add broken oppgave to examples in demo

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import { Heading, Label } from '@navikt/ds-react'
+import { Alert, Heading, Label } from '@navikt/ds-react'
 import Link from 'next/link'
 import React from 'react'
 
@@ -11,35 +11,44 @@ import { getServerEnv } from '../utils/env'
 const Home: NextPage = () => {
     return (
         <PageWrapper title="Digitalisering av Sykmeldinger">
-            <Heading size="medium">Digitalisering av Sykmeldinger</Heading>
-            <Label>Denne siden skal ikke vises i produksjon</Label>
+            <Alert variant="warning" className="m-4 max-w-prose">
+                <Heading size="medium">Digitalisering av Sykmeldinger</Heading>
+                <Label>Denne siden skal ikke vises i produksjon</Label>
+            </Alert>
 
-            <ul>
-                <li>
-                    <Link href="/oppgave/eksisterende">Eksempel utenlansk oppgave med eksisterende data</Link>
-                </li>
-                <li>
-                    <Link href="/oppgave/blank">Eksempel utenlansk oppgave uten eksisterende data</Link>
-                </li>
-                <li>
-                    <Link href="/oppgave/ferdigstilt">Eksempel utenlansk oppgave med status Ferdigstilt</Link>
-                </li>
-                <li>
-                    <Link href="/oppgave/finnesikke">Eksempel utenlansk oppgave med status FinnesIkke</Link>
-                </li>
-                <li>
-                    <Link href="/oppgave/avvist">Eksempel utenlansk oppgave med status Avvist</Link>
-                </li>
-                <li>
-                    <Link href="/oppgave/ikkeensykmelding">Eksempel utenlansk oppgave med status IkkeEnSykmelding</Link>
-                </li>
-            </ul>
+            <div className="m-4">
+                <h2 className="mt-8 text-xl font-bold">Eksempler på oppgaver</h2>
+                <ul className="list-disc pl-8">
+                    <li>
+                        <Link href="/oppgave/eksisterende">Oppgave med eksisterende data</Link>
+                    </li>
+                    <li>
+                        <Link href="/oppgave/blank">Oppgave uten eksisterende data</Link>
+                    </li>
+                    <li>
+                        <Link href="/oppgave/ferdigstilt">Oppgave med status Ferdigstilt</Link>
+                    </li>
+                    <li>
+                        <Link href="/oppgave/finnesikke">Oppgave med status FinnesIkke</Link>
+                    </li>
+                    <li>
+                        <Link href="/oppgave/avvist">Oppgave med status Avvist</Link>
+                    </li>
+                    <li>
+                        <Link href="/oppgave/ikkeensykmelding">Oppgave med status IkkeEnSykmelding</Link>
+                    </li>
+                    <li>
+                        <Link href="/oppgave/bare-tull">Oppgave som ikke vil laste og vise en ukjent feil</Link>
+                    </li>
+                </ul>
+            </div>
         </PageWrapper>
     )
 }
 
 export const getServerSideProps = withAuthenticatedPage(async (context, accessToken) => {
-    if (getServerEnv().NEXT_PUBLIC_RUNTIME_ENVIRONMENT === 'production') {
+    const runtimeEnv = getServerEnv().NEXT_PUBLIC_RUNTIME_ENVIRONMENT
+    if (runtimeEnv === 'production' || runtimeEnv === 'dev') {
         return { notFound: true }
     }
 
