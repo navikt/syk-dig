978a8a698a8c83523ba9d4b49e68ab0088edde58
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-10-31 08:00:26 +0100

    Updated contact info

--- src/pages/oppgave/[oppgaveId].page.tsx
+++ src/pages/oppgave/[oppgaveId].page.tsx
@@ -66,7 +66,8 @@ function OppgaveError({ oppgaveId }: { oppgaveId: string }): ReactElement {
             <BodyShort spacing>{`Klarte ikke å laste oppgave med oppgave-id "${oppgaveId}".`}</BodyShort>
             <BodyShort>
                 Du kan klikke her for å <Link href="">oppfriske</Link> siden. Dersom problemet vedvarer kan du kontakte
-                Team Sykmelding på Teams!
+                Team Sykmelding på <a href="https://nav-it.slack.com/archives/CMA3XV997">Slack!</a> eller sende mail til
+                <a href="mailto:nav.sykmelding@nav.no">nav.sykmelding@nav.no</a>
             </BodyShort>
         </Alert>
     )
