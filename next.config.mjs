5fddd1a3de32eedcdd6aee5e3a360192c06ab39e
Karl J. Overå <k@rl.run>
2023-11-24 12:01:17 +0100

    fix csp

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,7 @@
 import bundleAnalyzer from '@next/bundle-analyzer'
 
-const ContentSecurityPolicy = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' https://*.nav.no;
-    style-src 'self' 'unsafe-inline' https://cdn.nav.no;
-    img-src 'self' blob: data:;
-    font-src 'self' https://cdn.nav.no;
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    block-all-mixed-content;
-    upgrade-insecure-requests;
-    worker-src 'self';
-    connect-src 'self' https://*.nav.no;
-`
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        // if (process.env.NODE_ENV === 'development') return []
-
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
-                    },
-                ],
-            },
-        ]
-    },
     output: 'standalone',
     reactStrictMode: true,
     assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
---
+++ src/middleware.ts
@@ -0,0 +1,76 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { randomSessionId } from '@unleash/nextjs'
+
+import { UNLEASH_COOKIE_NAME } from './toggles/rsc'
+
+/**
+ * Middleware is run on every document request, it handles CSP,
+ * generates unleash session id, and gives the layout.tsx a
+ * path to redirect on for auth
+ */
+export default async function middleware(req: NextRequest): Promise<NextResponse> {
+    const requestHeaders = new Headers(req.headers)
+
+    const [cspHeader, nonce] = createCsp()
+    requestHeaders.set('x-nonce', nonce)
+    requestHeaders.set('Content-Security-Policy', cspHeader)
+
+    const res = NextResponse.next({
+        request: {
+            headers: requestHeaders,
+        },
+    })
+
+    const url: URL = new URL(req.url)
+    res.headers.set('x-path', url.pathname + url.search)
+    res.headers.set('Content-Security-Policy', cspHeader)
+
+    const existingCookie = req.cookies.get(UNLEASH_COOKIE_NAME)
+    if (existingCookie != null) {
+        return res
+    }
+
+    res.cookies.set(UNLEASH_COOKIE_NAME, randomSessionId())
+
+    return res
+}
+
+function createCsp(): [string, string] {
+    const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+    const cspHeader = `
+        default-src 'self';
+        script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+        style-src 'self' 'nonce-${nonce}' https://cdn.nav.no;
+        img-src 'self' blob: data:;
+        font-src 'self' https://cdn.nav.no;
+        object-src 'none';
+        base-uri 'self';
+        form-action 'self';
+        frame-ancestors 'none';
+        block-all-mixed-content;
+        upgrade-insecure-requests;
+    `
+        .replace(/\s{2,}/g, ' ')
+        .trim()
+
+    return [cspHeader, nonce]
+}
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        {
+            source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+            missing: [
+                { type: 'header', key: 'next-router-prefetch' },
+                { type: 'header', key: 'purpose', value: 'prefetch' },
+            ],
+        },
+    ],
+}
