2fe9cff6bca3fa3741b4ca164ab86f4f53b86e8a
Karl J. Overå <k@rl.run>
2024-04-15 16:21:38 +0200

    chore(deps): aksel v6

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
     reactStrictMode: true,
     assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
     experimental: {
+        optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
         serverComponentsExternalPackages: ['@navikt/next-logger', 'next-logger'],
     },
     eslint: {
