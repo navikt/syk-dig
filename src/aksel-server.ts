0e8600aaa2b695cda6b630cd58dcbff11fccf47d
Karl J. Overå <k@rl.run>
2024-04-15 16:21:38 +0200

    chore(deps): aksel v6

--- src/aksel-server.ts
+++
@@ -1,4 +0,0 @@
-export * from '@navikt/ds-react/esm/guide-panel'
-export * from '@navikt/ds-react/esm/typography'
-export * from '@navikt/ds-react/esm/link'
-export * from '@navikt/ds-react/esm/skeleton'
--- src/aksel.ts
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export * from '@navikt/ds-react/esm/alert'
-export * from '@navikt/ds-react/esm/button'
-export * from '@navikt/ds-react/esm/form/Select'
-export * from '@navikt/ds-react/esm/form/TextField'
-export * from '@navikt/ds-react/esm/internal-header'
-
-export { StethoscopeIcon, LineGraphStackedIcon, TableIcon } from '@navikt/aksel-icons'
