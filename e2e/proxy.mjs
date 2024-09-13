080263031b59fddece99f99fee2ce1baabc007cd
Karl J. Overå <k@rl.run>
2024-09-13 09:15:22 +0200

    chore: prebuild and run e2e with service container

--- e2e/proxy.mjs
+++
@@ -1,7 +0,0 @@
-import httpProxy from 'http-proxy'
-
-console.info('Starting proxy server')
-httpProxy
-    .createProxyServer({ target: 'http://app:3000' })
-    .listen(3000)
-    .on('start', () => console.info('Proxy server started'))
