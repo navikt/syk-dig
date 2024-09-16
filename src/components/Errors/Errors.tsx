718f9097733d375d66588081ac078bfa963ee175
Karl J. Overå <k@rl.run>
2024-09-16 15:31:43 +0200

    chore: log and display parse errors for nasjonal sykmeldinger
    Co-authored-by: Andreas <danduras@gmail.com>

--- src/components/Errors/Errors.tsx
+++ src/components/Errors/Errors.tsx
@@ -5,7 +5,6 @@ import { useFormContext } from 'react-hook-form'
 import { UtenlanskFormValues } from '../Sykmelding/SykmeldingForm'
 
 import { extractAllErrors } from './errorUtils'
-import styles from './Errors.module.css'
 
 function Errors(_: unknown, ref: ForwardedRef<HTMLDivElement>): ReactElement | null {
     const {
@@ -20,7 +19,7 @@ function Errors(_: unknown, ref: ForwardedRef<HTMLDivElement>): ReactElement | n
             size="medium"
             heading="Du må fylle ut disse feltene før du kan registrere sykmeldingen"
             ref={ref}
-            className={styles.errorSummary}
+            className="my-8 mx-4"
         >
             {errorSummary.map(({ name, message }) => (
                 <ErrorSummary.Item key={name} href={`#${name}`}>
