91bd3e50c7fa2c2008f15d5c1bdd45b1f7a87640
Karl J. Overå <k@rl.run>
2024-12-11 20:05:47 +0100

    chore(deps): next 15.1, react 19 and more

--- src/utils/test/apolloTestUtils.ts
+++ src/utils/test/apolloTestUtils.ts
@@ -1,12 +1,12 @@
-import { FetchResult, TypedDocumentNode } from '@apollo/client'
+import { FetchResult, TypedDocumentNode, Unmasked } from '@apollo/client'
 import { MockedResponse, ResultFunction } from '@apollo/client/testing'
 
 export function createMock<Query, Variables extends Record<string, unknown>>(mockedResponse: {
     request: { query: TypedDocumentNode<Query, Variables>; variables?: Variables }
-    result?: FetchResult<Query> | ResultFunction<FetchResult<Query>>
+    result?: FetchResult<Unmasked<Query>> | ResultFunction<FetchResult<Unmasked<Query>>, Variables>
     error?: Error
     delay?: number
-    newData?: ResultFunction<FetchResult<Query>>
+    newData?: ResultFunction<FetchResult<Unmasked<Query>>, Variables>
 }): MockedResponse<Query> {
     return mockedResponse
 }
