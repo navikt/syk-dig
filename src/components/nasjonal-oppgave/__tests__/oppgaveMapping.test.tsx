8a21ed5f338b49287f9ccc75e19dc4ce1102a1c5
Natalie Uranes <natalie.uranes@nav.no>
2025-02-13 18:41:02 +0100

    chore: skip api error tests

--- src/components/nasjonal-oppgave/__tests__/oppgaveMapping.test.tsx
+++ src/components/nasjonal-oppgave/__tests__/oppgaveMapping.test.tsx
@@ -135,7 +135,7 @@ describe('Mapping opppgave fetched from API', async () => {
 
         // 12 Behandler
         expect(screen.getByLabelText('12.1 Behandletdato')).toHaveDisplayValue(formatsmregDateShorthand('2025-01-14'))
-    }, 15000)
+    }, 20000)
 
     describe('NasjonalOppgaveStatus', () => {
         it('Should show status for nasjonalOppgave AVVIST', async () => {
