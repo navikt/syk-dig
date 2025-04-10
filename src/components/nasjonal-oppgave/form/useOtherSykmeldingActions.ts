83d2a43ed841a4a8e29862015d71b5e09eb2f369
Natalie Uranes <natalie.uranes@nav.no>
2025-04-10 11:55:11 +0200

    fix: til gosys mutation tuple types

--- src/components/nasjonal-oppgave/form/useOtherSykmeldingActions.ts
+++ src/components/nasjonal-oppgave/form/useOtherSykmeldingActions.ts
@@ -3,33 +3,37 @@ import { logger } from '@navikt/next-logger'
 
 import {
     AvvisNasjonalOppgaveDocument,
-    OppdatertSykmeldingStatus,
     AvvisNasjonalOppgaveMutation,
     AvvisNasjonalOppgaveMutationVariables,
     TilbakeTilGosysNasjonalDocument,
+    TilbakeTilGosysNasjonalMutation,
+    TilbakeTilGosysNasjonalMutationVariables,
 } from '../../../graphql/queries/graphql.generated'
 
 export function useTilbakeTilGosysNasjonal({
     onCompleted,
 }: {
     onCompleted?: () => void
-}): MutationTuple<OppdatertSykmeldingStatus, { oppgaveId: string }> {
-    return useMutation<OppdatertSykmeldingStatus, { oppgaveId: string }>(TilbakeTilGosysNasjonalDocument, {
-        onCompleted: () => {
-            onCompleted?.()
-        },
-        onError: (error, opts) => {
-            if (error.networkError) {
-                if ('response' in error.networkError) {
-                    logger.info(
-                        `Server responded with ${error.networkError.statusCode} (tilbake til gosys, ${opts?.variables?.oppgaveId}), squelching error log`,
-                    )
-                    return
+}): MutationTuple<TilbakeTilGosysNasjonalMutation, TilbakeTilGosysNasjonalMutationVariables> {
+    return useMutation<TilbakeTilGosysNasjonalMutation, TilbakeTilGosysNasjonalMutationVariables>(
+        TilbakeTilGosysNasjonalDocument,
+        {
+            onCompleted: () => {
+                onCompleted?.()
+            },
+            onError: (error, opts) => {
+                if (error.networkError) {
+                    if ('response' in error.networkError) {
+                        logger.info(
+                            `Server responded with ${error.networkError.statusCode} (tilbake til gosys, ${opts?.variables?.oppgaveId}), squelching error log`,
+                        )
+                        return
+                    }
+                    throw error
                 }
-                throw error
-            }
+            },
         },
-    })
+    )
 }
 
 export function useAvvisSykmeldingNasjonal({
