811ba1bccfcbbf7ef1a1337787a98d9c741be835
Karl J. Overå <k@rl.run>
2025-10-08 14:44:53 +0200

    chore(deps): bump all minors/patches

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
     output: 'standalone',
     reactStrictMode: true,
     assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
-    serverExternalPackages: ['@navikt/next-logger', 'next-logger', 'pino', 'graphql'],
+    serverExternalPackages: ['@navikt/next-logger', 'next-logger', 'pino', 'graphql', '@whatwg-node'],
     experimental: {
         optimizePackageImports: ['@navikt/ds-react', '@navikt/aksel-icons'],
     },
