307645fb0bc051045082bea2680b2de1a7e04ef4
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2025-11-03 10:11:01 +0100

    fix: add navEnhet to missing

--- src/components/Sykmelding/ActionSection/mutations/useTilbakeTilGosys.ts
+++ src/components/Sykmelding/ActionSection/mutations/useTilbakeTilGosys.ts
@@ -2,6 +2,7 @@ import { MutationResult, useMutation } from '@apollo/client'
 
 import { TilbakeTilGosysDocument, TilbakeTilGosysMutation } from '../../../../graphql/queries/graphql.generated'
 import { Location, useOppgaveParam } from '../../../../utils/useOppgaveParam'
+import { useModiaContext } from '../../../../modia/modia-context'
 
 type UseHandleTilbakeTilGosysOptions = {
     onCompleted?: () => void
@@ -10,12 +11,13 @@ type UseHandleTilbakeTilGosysOptions = {
 type UseHandleTilbakeTilGosys = [tilbakeTilGosys: () => void, result: MutationResult<TilbakeTilGosysMutation>]
 
 export function useHandleTilbakeTilGosys({ onCompleted }: UseHandleTilbakeTilGosysOptions): UseHandleTilbakeTilGosys {
+    const modiaContext = useModiaContext()
     const params = useOppgaveParam(Location.Utenlansk)
     const [tilbakeTilGosys, mutationResult] = useMutation(TilbakeTilGosysDocument)
 
     const mutation = async (): Promise<void> => {
         await tilbakeTilGosys({
-            variables: { oppgaveId: params.oppgaveId },
+            variables: { oppgaveId: params.oppgaveId, navEnhet: modiaContext.selectedEnhetId },
         })
         onCompleted?.()
     }
